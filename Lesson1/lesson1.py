b260ec8631c61d4dd1e2c18f41e8b766dd2d7b39
QuinnBrimley <QBR@Westlake.school.nz>
2024-06-17 09:43:08 +1200

    Update to lesson code 1

---
+++ Lesson1/lesson1.py
@@ -0,0 +1,24 @@
+print("Hello world!")
+num = 5
+print(num)
+name = "David"
+print(name)
+name_one = "Tim"
+
+x = 1
+y = 5
+z = x % y
+print(z)
+
+
+new_name = name_one + " Cook"
+print(new_name)
+
+name = str(input("Enter your name: "))
+age = int(input("Enter age: "))
+print(name + "s")
+print(age + 5)
+
+x = 1
+x = 23
+x = 3
