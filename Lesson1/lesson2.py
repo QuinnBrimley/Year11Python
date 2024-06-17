f32672501847119fbace81a67623b3d0addeec0c
QuinnBrimley <QBR@Westlake.school.nz>
2024-06-18 09:51:25 +1200

    Lesson 2 code

---
+++ Lesson1/lesson2.py
@@ -0,0 +1,14 @@
+#This is a comment!
+"""
+This is a block comment!
+"""
+#Comments are lines of code that are not excuted by python
+
+
+name = "Quinn"
+print(f"Hello {name}!") #How to print variables in a string
+
+my_decimal = 1.23454656546546456 #making a float
+my_decimal_two = 2.523454365246524
+
+print(f"float 1 + float 2 = {my_decimal+my_decimal_two:.5f}")
