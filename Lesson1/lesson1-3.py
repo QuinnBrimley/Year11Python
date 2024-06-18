7b135d5bc9b7d1bb0879ada347a92478fa047a66
QuinnBrimley <QBR@Westlake.school.nz>
2024-06-19 09:05:09 +1200

    added some code for floats

---
+++ Lesson1/lesson1-3.py
@@ -0,0 +1,14 @@
+str1 = "Kat"
+str2 = "Kat"
+
+print(str1 == str2) #Double equals does not assign a variable,  it checks if two variables are the same
+str2 = "Kate"
+print(str1 == str2) #Notice how it returns False? True and false are datatypes like integers and strings, True and false are called booleans
+str1 = str2 #Using one equals sign means we are assigning a variable.
+print(str1)
+
+
+x = 2 #is a integer
+y = 3 #is also a integer
+z = 2/3 #The result is a float
+print(int(z)) #What do you think is the result?
