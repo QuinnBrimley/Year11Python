c3a41718c88f23bf89a7541aef2eff64c69510bb
QuinnBrimley <QBR@Westlake.school.nz>
2024-06-19 08:36:01 +1200

    Updated to include some of the things we talked about in class

--- Lesson1/lesson1-2.py
+++ Lesson1/lesson1-2.py
@@ -1,13 +1,15 @@
 #This is a comment!
 #Comments are lines of code that are not excuted by python
 """
-This is a block comment!
+This is a block comment!s
 """
 
 
-
-name = "Mr Brimley"
+last_name = "billybob"
+print(f"Hello good sir {last_name}")
+name = "Mr Brimley "
 print(f"Hello {name}!") #How to print variables in a string
+print(name + last_name) #DO NOT DO THIS!!!!!!!!!! >:(
 
 my_decimal = 1.23454656546546456 #making a float
 my_decimal_two = 2.523454365246524
