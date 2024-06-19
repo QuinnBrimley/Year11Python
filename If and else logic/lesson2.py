3f6eccfa2bd9bbc58e781bbf2a70586204d47999
QuinnBrimley <QBR@Westlake.school.nz>
2024-06-19 16:35:10 +1200

    Added short ciruits to code

--- If and else logic/lesson2.py
+++ If and else logic/lesson2.py
@@ -29,6 +29,8 @@ else:
 
 """
 We can add a else if to check more conditons like this
+Brackets are not needed around the condtion however it can help
+with visibility and readability.
 
 if condition:
     do something
@@ -49,3 +51,31 @@ else:
 
 #Note that we can include as many elif we like, we just 
 #Need to add it on to our if and else structure.
+
+#Does changing and to or do anything?
+if age >= 18:
+    print("You are 18 years old or older")
+elif age <= 17 or age >=10:
+    print("You are between 17 and 10")
+else:
+    print("You are younger than 10")
+
+
+"""
+Short circuiting is when a if condition doesnt full complete
+its check
+"""
+
+a = False
+b = True
+# This won't print anything because `a` is False, so `b` is not evaluated
+if a and print("This won't be printed"):
+    print("Inside if block")
+
+
+a = True
+b = False
+
+# This won't print anything because `a` is True, so `b` is not evaluated
+if a or print("This won't be printed"):
+    print("Inside if block")
