3e6d9ddb301c0ae1462fc34bbbcf3e16e5e1b4e2
QuinnBrimley <QBR@Westlake.school.nz>
2024-06-19 15:04:01 +1200

    Update comments to reflect better what is happening in the rounding

--- Introduction/lesson1-2.py
+++ Introduction/lesson1-2.py
@@ -14,5 +14,5 @@ print(name + last_name) #DO NOT DO THIS!!!!!!!!!! >:(
 my_decimal = 1.23454656546546456 #making a float
 my_decimal_two = 2.523454365246524
 
-print(f"float 1 + float 2 = {my_decimal+my_decimal_two:.5f}") #The :.5f - the 5 can be replaced by any number 
+print(f"float 1 + float 2 = {my_decimal+my_decimal_two:.5f}") #The :.5f - the 5 can be replaced by any number to round
 
