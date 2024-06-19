338cf7ad72b44d5f0d355d7a4771343383369658
QuinnBrimley <QBR@Westlake.school.nz>
2024-06-19 15:02:15 +1200

    Boolean code added to lesson

--- Lesson1/lesson1-3.py
+++ Lesson1/lesson1-3.py
@@ -7,6 +7,10 @@ print(str1 == str2) #Notice how it returns False? True and false are datatypes l
 str1 = str2 #Using one equals sign means we are assigning a variable.
 print(str1)
 
+#These are boolean values, they are either True (1) or False (0)
+booleanTrue = True 
+booleanFalse = False
+
 
 x = 2 #is a integer
 y = 3 #is also a integer
